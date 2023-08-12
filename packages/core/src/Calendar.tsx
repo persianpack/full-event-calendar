b7cc18ec4e791462f10ea69f89299d0b65d7890b
amirkian007 <amircc1200@gmail.com>
2023-08-12 23:44:43 +0330

    USE new event and calendar api

--- packages/core/src/Calendar.tsx
+++ packages/core/src/Calendar.tsx
@@ -1,80 +1,52 @@
-import { CalendarApi } from './api/CalendarApi.js'
+import { EventCalendar } from './api/CalendarApi.js'
 import { createSignal } from 'solid-js'
+import { useCounter } from './contex-injector/contex.js'
 import { CounterProvider } from './contex-injector/contex.jsx'
-
-import { render } from 'solid-js/web'
+import { hydrate, render } from 'solid-js/web'
 import useRedux from './store/useRedux'
 import reduxStore from './store/store'
 import actions from './store/actions'
-import { useCounter } from './contex-injector/contex.js'
+import { App } from './App.js'
 
-function App() {
-  const [count, setCount] = createSignal(0)
-  const data = useCounter()
-
+function CalendarRoot(props: any) {
   return (
-    <>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count()}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">Click on the Vite and Solid logos to learn more</p>
-      <div></div>
-    </>
-  )
-}
-
-function CalendarRoot() {
-  const [store, { addTodo, toggleTodo }] = useRedux(reduxStore, actions)
-  addTodo('sdsds')
-  console.log(store)
-  return (
-    <CounterProvider store={store}>
+    <CounterProvider store={props.store}>
       <App />
     </CounterProvider>
   )
 }
 
-export class EventCalendar implements CalendarApi {
+export class Calendar extends EventCalendar {
   private targetElement: HTMLElement
-  private calendaerEventList: PickType<EventCalendarOptions, 'events'>
-  //   private timeZone : PickType<EventCalendarOptions,'timeZone'>;
-  //   private dailyGridOptions : PickType<EventCalendarOptions,'dailyGridOptions'>;
-
+  // private calendaerEventList: PickType<EventCalendarOptions, 'events'>
+  // private timeZone : PickType<EventCalendarOptions,'timeZone'>;
+  // private dailyGridOptions : PickType<EventCalendarOptions,'dailyGridOptions'>;
   constructor(targetElement: HTMLElement, eventCalendarOptions: EventCalendarOptions) {
-    // super()
+    super(eventCalendarOptions)
     this.targetElement = targetElement
-    this.calendaerEventList = eventCalendarOptions.events
     // this.dailyGridOptions = eventCalendarOptions.dailyGridOptions
     // this.timeZone = eventCalendarOptions.timeZone
   }
-  prevDay(): void {
-    // dispatch to store manager
-  }
-  nextDay(): void {
-    // dispatch to store manager
-  }
-  getDate(): Date {
-    return new Date()
-  }
-  today(): void {}
+
   render() {
-    render(() => <CalendarRoot />, this.targetElement)
+    render(() => <CalendarRoot store={this.storeManager} />, this.targetElement)
     // function hydrate(fn: () => JSX.Element, node: MountableElement): () => void;
+  }
+  refresh() {
+    hydrate(() => <CalendarRoot store={this.storeManager} />, this.targetElement)
   }
 }
 
 interface InputeEvent {
   start: Date
   end: Date
-  name: string
+  id: any
 }
 interface dailyGridOptions {
   selectedDate: Date
 }
 type CalnedarMode = 'daily'
+
 interface EventCalendarOptions {
   events: InputeEvent[]
   // timeZone ?: string;
