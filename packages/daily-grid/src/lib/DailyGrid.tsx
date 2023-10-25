8f244e714d6685537b602b4a092d8fa00e00e978
amirkian007 <amircc1200@gmail.com>
2023-10-25 20:52:14 +0330

    USE chache in dailyGrid

--- packages/daily-grid/src/lib/DailyGrid.tsx
+++ packages/daily-grid/src/lib/DailyGrid.tsx
@@ -1,9 +1,9 @@
-import { FComponent } from '@full-event-calendar/shared-ts'
+import { EventClass, FComponent } from '@full-event-calendar/shared-ts'
 import { DailyHeader } from './DailyHeader/DailyHeader'
 // import { DailyHeaderProps } from './DailyHeader/DailyHeader'
 import { BasicGrid } from '@full-event-calendar/basid-grid'
 import { BasicGridProps } from '@full-event-calendar/basid-grid'
-import { mergeProps } from 'solid-js'
+import { createMemo, mergeProps } from 'solid-js'
 interface DailyGridpProps extends BasicGridProps {
   initialDate?: Date
   onDateChange?: (d: Date) => void
@@ -22,8 +22,25 @@ const defaultProps = {
 
 // type EventCalendarOptionsss = { [K in keyof DailyGridpProps]-?: DailyGridpProps[K] }
 
+function getEventForAdate(events: EventClass[], targetDate: Date) {
+  const filteredERvents = events.filter((event) => {
+    const conditain1 =
+      event.start.getFullYear() === targetDate.getFullYear() &&
+      event.start.getMonth() === targetDate.getMonth() &&
+      event.start.getDate() === targetDate.getDate()
+    const conditain2 =
+      event.end.getFullYear() === targetDate.getFullYear() &&
+      event.end.getMonth() === targetDate.getMonth() &&
+      event.end.getDate() === targetDate.getDate()
+    return conditain1 || conditain2
+  })
+  return filteredERvents
+}
+
 export const DailyGrid: FComponent<DailyGridpProps> = (props) => {
   const mergedPorps = mergeProps(defaultProps, props)
+
+  const filteredEvents = createMemo(() => getEventForAdate(mergedPorps.events, mergedPorps.initialDate))
 
   return (
     <>
@@ -33,7 +50,7 @@ export const DailyGrid: FComponent<DailyGridpProps> = (props) => {
         calendar={mergedPorps.calendar}
         onDateChange={mergedPorps.onDateChange}
       />
-      <BasicGrid events={mergedPorps.events} onEventUpdate={mergedPorps.onEventUpdate} />
+      <BasicGrid events={filteredEvents()} onEventUpdate={mergedPorps.onEventUpdate} />
     </>
   )
 }
