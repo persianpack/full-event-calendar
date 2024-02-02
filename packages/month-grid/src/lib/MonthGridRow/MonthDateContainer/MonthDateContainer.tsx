97afb5cb0c7b72cf967f55125683959ff5e1002c
amirkian007 <amircc1200@gmail.com>
2024-02-02 18:09:29 +0330

    fix(month): fix dragging bugs

--- packages/month-grid/src/lib/MonthGridRow/MonthDateContainer/MonthDateContainer.tsx
+++ packages/month-grid/src/lib/MonthGridRow/MonthDateContainer/MonthDateContainer.tsx
@@ -5,28 +5,44 @@ import { formatNumber, getMonthName } from '@full-event-calendar/utils'
 import './MonthDateContainer.scss'
 
 interface MonthDateContainerProps {
-    locale: string
-    monthRowIndex: number // props.monthRowIndex
-    monthRowDates: MonthDateObject[] // monthRowArr
-    calendar: string // props
-    dragClick: any // 
-    monthDateMouseDown: any //
-    onMouseEnter: any //
+  locale: string
+  monthRowIndex: number // props.monthRowIndex
+  monthRowDates: MonthDateObject[] // monthRowArr
+  calendar: string // props
+  dragClick: any //
+  monthDateMouseDown: any //
+  onMouseEnter: any //
 }
 
 export const MonthDateContainer: FComponent<MonthDateContainerProps> = (props) => {
-
   function stopDefault(e: MouseEvent) {
     e.stopPropagation()
     e.preventDefault()
   }
 
-return (
+  function mouseDownSome(date: Date, e: MouseEvent) {
+    let hasMouseMoved = false
+    function handelMouseMove() {
+      if (!hasMouseMoved) {
+        props.monthDateMouseDown(date, e)
+        hasMouseMoved = true
+      }
+      document.removeEventListener('mousemove', handelMouseMove)
+    }
+    function handelMouseUp() {
+      document.removeEventListener('mouseup', handelMouseUp)
+      document.removeEventListener('mousemove', handelMouseMove)
+    }
+    document.addEventListener('mousemove', handelMouseMove)
+    document.addEventListener('mouseup', handelMouseUp)
+  }
+  // props.monthDateMouseDown
+  return (
     <>
-     <For each={props.monthRowDates}>
+      <For each={props.monthRowDates}>
         {(date, i) => (
           <div
-            onmousedown={[props.monthDateMouseDown, date.date]}
+            onmousedown={[mouseDownSome, date.date]}
             class="month-container"
             onmousemove={() => props.onMouseEnter(date.date)}
           >
@@ -43,13 +59,18 @@ return (
   )
 }
 
-function isDateInsideMonth(date: MonthDateObject, index: number, monthRowIndex: number, monthRowArr: MonthDateObject[]) {
-    if (date.isDateInsideMonth) {
-      if (monthRowIndex === 0) {
-        return date.month != monthRowArr[index + 1]?.month ? 'month-day-out' : 'month-day-out-no-name'
-      } else if (date.isDateInsideMonth) {
-        return date.month != monthRowArr[index - 1]?.month ? 'month-day-out' : 'month-day-out-no-name'
-      }
+function isDateInsideMonth(
+  date: MonthDateObject,
+  index: number,
+  monthRowIndex: number,
+  monthRowArr: MonthDateObject[]
+) {
+  if (date.isDateInsideMonth) {
+    if (monthRowIndex === 0) {
+      return date.month != monthRowArr[index + 1]?.month ? 'month-day-out' : 'month-day-out-no-name'
+    } else if (date.isDateInsideMonth) {
+      return date.month != monthRowArr[index - 1]?.month ? 'month-day-out' : 'month-day-out-no-name'
     }
-    return ''
   }
+  return ''
+}
